3be6660c556134df0137ebb7bf1ebdd2298122d9
Shubham Jain <shubham.jain.1@gmail.com>
2021-05-08 14:06:28 +0530

    Disable JS execution by default

--- svg-loader.js
+++ svg-loader.js
@@ -33,19 +33,60 @@ const setCache = (url, data, cacheOpt) => {
         }));
     } catch (e) {
         return;
-    }
-    
+    }  
 };
 
-const renderBody = (elem, body) => {
+const DOM_EVENTS = [];
+const getAllEventNames = () => {
+    if (DOM_EVENTS.length) {
+        return DOM_EVENTS;
+    }
+
+    for (const prop in document.head) {
+        if(prop.startsWith("on")) {
+            DOM_EVENTS.push(prop);
+        }
+    }
+
+    return DOM_EVENTS;
+};
+
+const renderBody = (elem, jsEnabled, body) => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(body, "text/html");
     const fragment = doc.querySelector("svg");
+
+    const eventNames = getAllEventNames();
 
     // When svg-loader is loading in the same element, it's
     // important to keep track of original properties.
     const elemAttributesSet = elem.getAttribute("data-attributes-set");
     const attributesSet = elemAttributesSet ? new Set(elemAttributesSet.split(",")) : new Set();
+
+    // Unless explicitly set, remove possible JS code
+    Array.from(doc.querySelectorAll("*")).forEach((elem) => {
+        if (elem.tagName === "script") {
+            elem.remove();
+            return;
+        }
+
+        for (let i = 0; i < elem.attributes.length; i++) {
+            const {
+                name,
+                value
+            } = elem.attributes[i];
+
+            if (eventNames.includes(name.toLowerCase()) && !jsEnabled) {
+                elem.removeAttribute(name);
+                continue;
+            }
+
+            if (["href", "xlink:href"].includes(name) && value.startsWith("javascript")) {
+                elem.removeAttribute(name);
+                continue;
+            }
+        }
+    });
 
     for (let i = 0; i < fragment.attributes.length; i++) {
         const {
@@ -64,7 +105,6 @@ const renderBody = (elem, body) => {
     elem.setAttribute("data-attributes-set", Array.from(attributesSet).join(","));
     elem.setAttribute("data-rendered", true);
 
-    
     elem.innerHTML = fragment.innerHTML;
 };
 
@@ -75,15 +115,19 @@ const renderIcon = async (elem) => {
     const src = elem.getAttribute("data-src");
     const cacheOpt = elem.getAttribute("data-cache");
 
+    const jsEnabled = elem.hasAttribute("data-js-enabled");
+
     const lsCache = await isCacheAvailable(src);
     const isCachingEnabled = cacheOpt !== "disabled";
+
+    const renderBodyCb = renderBody.bind(this, elem, jsEnabled);
 
     // Memory cache optimizes same icon requested multiple
     // times on the page
     if (memoryCache[src] || (isCachingEnabled && lsCache)) {
         const cache = memoryCache[src] || lsCache;
 
-        renderBody(elem, cache);
+        renderBodyCb(cache);
     } else {
         // If the same icon is being requested to rendered
         // avoid firing multiple XHRs
@@ -103,7 +147,7 @@ const renderIcon = async (elem) => {
 
                 memoryCache[src] = body;
 
-                renderBody(elem, body);
+                renderBodyCb(body);
             })
             .finally(() => {
                 delete requestsInProgress[src];
@@ -156,12 +200,13 @@ const addObservers = () => {
         const shouldTriggerRender = mutationRecords.some(
             (record) => Array.from(record.addedNodes).some(
                 (elem) => elem.nodeType === Node.ELEMENT_NODE
-                    && ((elem.getAttribute("data-src") && !elem.getAttribute("data-rendered"))
-                        || elem.querySelector("svg[data-src]:not([data-rendered])"))
+                    && ((elem.getAttribute("data-src") && !elem.getAttribute("data-rendered")) // Check if the element needs to be rendered
+                        || elem.querySelector("svg[data-src]:not([data-rendered])")) // Check if any of the element's children need to be rendered
             )
         );
 
-        // If any node is added, render all new nodes
+        // If any node is added, render all new nodes because the nodes that have already
+        // been rendered won't be rendered again.
         if (shouldTriggerRender){
             renderAllSVGs();
         }
